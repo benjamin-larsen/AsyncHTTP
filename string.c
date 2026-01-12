59c25e988c6c303e851693fe19b8247fda4bf441
Benjamin <33324686+benjamin-larsen@users.noreply.github.com>
2026-01-12 14:06:02 +0100

    Expand String Library
    Added String Equals that compares two strings, and Split String that splits a string with a delimiter

--- string.c
+++ string.c
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include <stdint.h>
+#include <string.h>
 #include <stdlib.h>
 
 struct longString {
@@ -53,6 +54,61 @@ bool IsEmptyString(union string str) {
 void FreeString(union string *str) {
     if (str == nullptr) return;
     if (str->shortStr.len > 0) return;
+    if (str->longStr.buf == nullptr) return;
 
     free(str->longStr.buf);
+}
+
+bool StringEquals(union string strA, union string strB) {
+    uint8_t *bufA = GetStringBuf(&strA);
+    uint32_t lenA = GetStringLen(&strA);
+
+    uint8_t *bufB = GetStringBuf(&strB);
+    uint32_t lenB = GetStringLen(&strB);
+
+    if (lenA !=  lenB) return false;
+
+    return memcmp(bufA, bufB, lenA) == 0;
+}
+
+// Splits the string from source (leaving the first part in the dest) and puts remainder into src
+// Does not do short string optimization, dest gets the same type (short/long) as src.
+// Returns whether it was successful
+bool SplitString(union string *src, union string *dest, uint8_t delimiter) {
+    if (src == nullptr || dest == nullptr) return false;
+
+    uint8_t *buf = GetStringBuf(src);
+    uint32_t len = GetStringLen(src);
+
+    if (buf == NULL || len == 0) return false;
+
+    unsigned char *bounds = memchr(buf, delimiter, len);
+
+    if (bounds == NULL || bounds < buf) return false;
+
+    uint32_t size = (char*)bounds - (char*)buf;
+
+    if (src->shortStr.len > 0) {
+        struct shortString destStr = { .len = size };
+        memcpy(destStr.buf, src->shortStr.buf, size);
+
+        dest->shortStr = destStr;
+
+        // 1 to remove the delimiter itself
+        src->shortStr.len = len - size - 1;
+        memmove(
+            src->shortStr.buf,
+            (char *)src->shortStr.buf + size + 1,
+            src->shortStr.len
+        );
+    } else {
+        dest->longStr = (struct longString){ .len = size, .buf = buf };
+
+        src->longStr = (struct longString){
+            .len = len - size - 1,
+            .buf = (char *)buf + size + 1
+        };
+    }
+
+    return true;
 }
