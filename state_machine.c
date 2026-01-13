ebb32bb9077171444acbbe341fc4a510971d288a
Benjamin <33324686+benjamin-larsen@users.noreply.github.com>
2026-01-13 12:18:54 +0100

    Async State Machine

---
+++ state_machine.c
@@ -0,0 +1,116 @@
+﻿// Async State Machines
+
+#pragma once
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// First Parameter is a parameter that can be passed in.
+// Returns a ptr to be saved as Async State
+// Can pass in pointer to stack here, as constructor is called immediately on AwaitAsync.
+typedef void *(*async_constructor)(void *);
+
+// Parameter is ptr to Async State, this function is responsible for freeing
+typedef void (*async_destructor)(void *);
+
+// Parameter is ptr to Async State
+// Returns whether subroutine ended
+typedef bool (*async_subroutine)(void *);
+
+struct async_descriptor {
+    async_constructor constructor;
+    async_destructor destructor;
+    async_subroutine subroutine;
+};
+
+struct async_state {
+    struct async_descriptor descriptor;
+    void *state;
+    struct async_state *awaiting;
+};
+
+_Thread_local struct async_state *currentAsync = NULL;
+_Thread_local struct async_state *waitingAsync = NULL;
+
+const struct async_descriptor nullAsync = {
+    .constructor = NULL,
+    .destructor = NULL,
+    .subroutine = NULL
+};
+
+bool IsValidDescriptor(struct async_descriptor descriptor) {
+    if (descriptor.constructor == NULL) return false;
+    if (descriptor.destructor == NULL) return false;
+    if (descriptor.subroutine == NULL) return false;
+
+    return true;
+}
+
+// for IO operations structs must start with a io_result object
+
+void RunAsync(struct async_state *machineState) {
+    if (machineState == NULL) return;
+    if (!IsValidDescriptor(machineState->descriptor)) return;
+    if (machineState->state == NULL) return;
+
+    // Defer execution until this is done.
+    // Expects no more than one more async to be scheduled at once
+    if (currentAsync != NULL) {
+        if (waitingAsync != NULL) {
+            fprintf(stderr, "critical error: two async called at once.");
+            // perhaps kill the currentAsync, waitingAsync and machineState async
+            return;
+        }
+
+        waitingAsync = machineState;
+        return;
+    }
+
+    currentAsync = machineState;
+
+    bool finished = machineState->descriptor.subroutine(machineState->state);
+
+    currentAsync = NULL;
+    struct async_state *awaiting = NULL;
+
+    if (finished) {
+        awaiting = machineState->awaiting;
+
+        machineState->descriptor.destructor(machineState->state);
+        free(machineState);
+    }
+
+    // Run waiting async, before any others
+    if (waitingAsync != NULL) {
+        // Reset waiting async before running.
+        struct async_state *waitedAsync = waitingAsync;
+        waitingAsync = NULL;
+
+        RunAsync(waitedAsync);
+    }
+
+    if (awaiting == NULL) return;
+
+    RunAsync(awaiting);
+}
+
+void StartAsync(struct async_state *machineState, void *constructParam) {
+    if (machineState == NULL) return;
+    if (!IsValidDescriptor(machineState->descriptor)) return;
+
+    if (currentAsync != NULL) {
+        machineState->awaiting = currentAsync;
+    }
+
+    machineState->state = machineState->descriptor.constructor(constructParam);
+
+    RunAsync(machineState);
+}
+
+void AwaitAsync(struct async_descriptor descriptor, void *constructParam) {
+    struct async_state *machineState = calloc(1, sizeof(struct async_state));
+    machineState->descriptor = descriptor;
+
+    StartAsync(machineState, constructParam);
+}
---
+++ test.c
@@ -0,0 +1,104 @@
+﻿#include "./state_machine.c"
+#include <stdio.h>
+#include <stdlib.h>
+
+enum httpStage {
+    A,
+    B
+};
+
+struct otherState {
+    enum httpStage stage;
+    uint32_t nesting;
+};
+
+void *otherConstructor(void *param) {
+    struct otherState *state = calloc(1, sizeof(struct otherState));
+
+    state->stage = A;
+    state->nesting = *(uint32_t *)param;
+
+    return state;
+}
+
+void otherDestructor(void *state) {
+    return free(state);
+}
+
+extern const struct async_descriptor otherAsync;
+
+bool runOther(struct otherState *state) {
+    switch (state->stage) {
+        case A: {
+            state->stage = B;
+
+            uint32_t nesting = state->nesting;
+            printf("Running Other\n");
+            if (nesting > 0) {
+                uint32_t nextNesting = state->nesting - 1;
+                AwaitAsync(otherAsync, &nextNesting);
+                return false;
+            }
+
+            printf("Finished Other Instant\n");
+            return true;
+        }
+        case B: {
+            printf("Finished Other\n");
+            return true;
+        }
+    }
+
+    return true;
+}
+
+const struct async_descriptor otherAsync = {
+    .constructor = otherConstructor,
+    .destructor = otherDestructor,
+    .subroutine = runOther,
+};
+
+struct httpState {
+    enum httpStage stage;
+};
+
+void *httpConstructor(void *param) {
+    struct httpState *state = calloc(1, sizeof(struct httpState));
+    state->stage = A;
+
+    printf("%p\n", state);
+    return state;
+}
+
+void httpDestructor(void *state) {
+    return free(state);
+}
+
+bool runHttp(struct httpState *state) {
+    printf("Stage: %u\n", state->stage);
+
+    switch (state->stage) {
+        case A: {
+            state->stage = B;
+            uint32_t nesting = 5;
+            AwaitAsync(otherAsync, &nesting);
+            return false;
+        }
+
+        case B: {
+            return true;
+        }
+    }
+}
+
+const struct async_descriptor httpAsync = {
+    .constructor = httpConstructor,
+    .destructor = httpDestructor,
+    .subroutine = runHttp,
+};
+
+int main(void) {
+    printf("Starting %p\n", httpAsync.subroutine);
+    AwaitAsync(httpAsync, NULL);
+    return 0;
+}
